ffaea89def7ee2eca43046504464a0def6264d08
Barun Ghosh <barunonline678@gmail.com>
2020-04-07 19:31:07 +0530

    Initial commit 🙏

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+//import { google } from "google-maps";
+/*
+ *import { User } from './User';
+ *import { Company } from './Company';
+ */
+
+
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
+
+
+
+/*
+ *const user = new User();
+ *const company = new Company();
+ *
+ *console.log(user);
+ *console.log(company);
+ */
